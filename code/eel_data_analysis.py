dd4ba327e425f7b56652bb64fbccbf04302b1fec
Sarah Eisele <zxoyx50@student.uni-tuebingen.de>
2025-05-05 09:21:12 +0200

    start and end bin are percentually calculated and not disregarded, fixed some mistakes

--- code/eel_data_analysis.py
+++ code/eel_data_analysis.py
@@ -19,7 +19,6 @@ This script calculates the number of detected peaks per minute/hour and plots th
 # TODO: make main function
 # TODO: account for amount of recordings that contribute to each bin ?
 # TODO: implement case that there are no peaks detected (no file created or empty file)
-# ask patrick if i should not discard start and end bins but assign prozentual soviele peaks wie zeit in dieser bin war?
 
 # TODO: plot amplitude over time?/for individuals?
 
@@ -37,7 +36,11 @@ This script calculates the number of detected peaks per minute/hour and plots th
 
 # Wednesday
 # function geschrieben die checkt ob recordings subsequent sind und paths in recording sessions speichert
-# todo: function in peaks_over_time einfügen
+
+# Friday
+# made it work that start and end bin are percentually calculated and not disregarded
+# fixed mistakes
+# started implementing session function in peaks over time
 
 # %%
 from rich.console import Console
@@ -46,7 +49,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 from audioio.audioloader import AudioLoader
-from IPython import embed
+# from IPython import embed
 
 # Initialize console for logging
 con = Console()
@@ -175,12 +178,14 @@ def load_peaks(datapath):
     return sorted(npz_path_list)
 
 
+# %%
 # Calculate how many peaks per time interval
 def peaks_over_time(
     bin_size: int,
     datapath: Path,
-    file_paths: list,
-    min_per_rec: int = 5,  # minutes per recording
+    paths: dict,  # TODO: make that it works for dict
+    # TODO: iterate over sessions in peaks_over_time
+    # TODO: calculate end time +5 min??
 ):
     """
     Calculate number of peaks per minute.
@@ -189,71 +194,90 @@ def peaks_over_time(
     num_bins = int(24 * 60 / bin_size)  # 24 hours * 60 minutes / bin size in minutes
 
     # Initialize array to store number of peaks per bin
-    peaks_per_bin = np.full(num_bins, np.nan)
+    peaks_per_bin = np.zeros(num_bins)
+
+    # get time of first and last file of recording
+    start_time, end_time = get_timestamps(paths)
+
+    # calculate start and end bin (TODO: dont hardcode)
+    start_bin = start_time.hour
+    end_bin = end_time.hour
 
     # Iterate over each npz file independently to load it
     if datapath.is_file():
         with np.load(datapath) as data:
             # only use peaks that have been predicted as valid peaks
             valid_peaks = data["peaks"][data["predicted_labels"] == 1]
+            # TODO: insert function from below
 
     # Recursively find all .npz files in the directory and subdirectories
     elif datapath.is_dir():
-        for i, file in enumerate(datapath.rglob("*.npz")):
-            # TODO: make this into a function ?
+        for file in datapath.rglob("*.npz"):
+            # TODO: make this into a function
             with np.load(file) as data:
-                # extract sample rate
-                sample_rate = data["rate"]
-                # --------------
-                ###
-                # get time of first and last file of recording
-                start_time, end_time = get_timestamps(file_paths)
-
-                # calculate the difference in seconds to next bin
-                start_diff_seconds = timedelta(
-                    minutes=bin_size - (start_time.minute % bin_size),
-                    seconds=-start_time.second,
-                ).total_seconds()  # just take timedelta directly
-                end_diff_seconds = timedelta(
-                    minutes=end_time.minute % bin_size, seconds=end_time.second
-                ).total_seconds()
-
-                # calculate how many indices to discard at start and end of each session(folder)
-                start_diff_idx = int(start_diff_seconds * sample_rate)
-                end_diff_idx = int(end_diff_seconds * sample_rate)
-
-                # get correct start bin for this file
-                if start_time.minute > 0:
-                    # round up to the next bin if not already on a bin
-                    start_bin = start_time.hour + 1
-                else:
-                    start_bin = start_time.hour
-
-                # embed()
-                # -------------
-                ###
-                # data points per minute (sample rate in Hz * 60 sec/min)
-                points_per_min = sample_rate * 60
-
-                # calculate data points per bin
-                points_per_bin = points_per_min * bin_size
-
-                ###
                 # only take peaks that have been predicted as valid peaks
                 valid_peaks = data["centers"][data["predicted_labels"] == 1]
 
-                # loop over all peaks in file
-                for peak in valid_peaks:
-                    # discard peaks at start and end of rec where not whole length of bin is covered by data
-                    if (i == 0 and peak <= start_diff_idx) or (
-                        i == -1 and peak >= end_diff_idx
-                    ):
-                        continue
-                    else:
+                # extract sample rate
+                sample_rate = int(data["rate"])  # int
+
+                # calculate data points per minute (sample rate in Hz * 60 sec/min)
+                idx_per_min = sample_rate * 60
+
+                # calculate data points per bin
+                idx_per_bin = idx_per_min * bin_size
+
+                # case that start and end of a recording session are in the same hour
+                if start_time.hour == end_time.hour:
+                    # calculate the difference of start and end time in seconds
+                    diff_seconds = timedelta(
+                        minutes=end_time.minute - start_time.minute,
+                        seconds=end_time.second - start_time.second,
+                    ).total_seconds()
+
+                    # convert to number of indices in this time window
+                    diff_idx = int(diff_seconds * sample_rate)
+
+                    # calculate how many percent of the bin is covered by data
+                    diff_percent = diff_idx / idx_per_bin
+
+                    # loop over all peaks in file
+                    for peak in valid_peaks:
+                        # assign peaks to bin
+                        global_peak_idx = int(peak // idx_per_bin) + start_bin
+                        # add correct number of peaks to bin
+                        peaks_per_bin[global_peak_idx] += diff_percent
+
+                # case that start and end of a recording session are in different hours
+                else:
+                    # calculate the difference to next/last bin in seconds
+                    start_diff_seconds = timedelta(
+                        minutes=bin_size - (start_time.minute % bin_size),
+                        seconds=-start_time.second,
+                    ).total_seconds()
+                    end_diff_seconds = timedelta(
+                        minutes=end_time.minute % bin_size, seconds=end_time.second
+                    ).total_seconds()
+
+                    # calculate how many indices to discard at start and end of each session(folder)
+                    start_diff_idx = int(start_diff_seconds * sample_rate)
+                    end_diff_idx = int(end_diff_seconds * sample_rate)
+
+                    # calculate the percentage of start_diff_idx relative to points_per_bin
+                    start_diff_percent = start_diff_idx / idx_per_bin
+                    end_diff_percent = end_diff_idx / idx_per_bin
+
+                    # loop over all peaks in file
+                    for peak in valid_peaks:
                         # assign peaks to bins
-                        global_peak_idx = int(peak // points_per_bin) + start_bin
-                        # add peak to corresponding bin
-                        peaks_per_bin[global_peak_idx] += 1
+                        global_peak_idx = int(peak // idx_per_bin) + start_bin
+                        # check if bin is at start or end of recording session, add correct number of peaks to bins
+                        if global_peak_idx == start_bin:
+                            peaks_per_bin[global_peak_idx] += start_diff_percent
+                        elif global_peak_idx == end_bin:
+                            peaks_per_bin[global_peak_idx] += end_diff_percent
+                        else:
+                            peaks_per_bin[global_peak_idx] += 1
 
     return peaks_per_bin
 
@@ -262,6 +286,10 @@ def check_sessions(
     file_paths: list,
     max_difference: int = 6,  # min
 ):
+    """
+    Check if recordings are subsequent and store seperate recording sessions in a dictionary.
+    """
+    # TODO: implement to check for missing files but not new rec session
     # extract timestamps from filepaths
     dt_list = []
     rec_sessions = {}
@@ -271,39 +299,68 @@ def check_sessions(
 
         # append dict for first session
         if i == 0:
+            rec_sessions[f"rec session {i}"] = []
             dt_list.append(dt.minute)
-            rec_sessions.append({f"rec session {i}": path})
 
         # check if time stamps are consecutive
         if i > 0:
             # access last saved time stamp of current session to check if current time is subsequent
             if dt.minute > (dt_list[-1] + max_difference):
-                rec_sessions[f"rec session {len(rec_sessions)}"] = None
+                rec_sessions[f"rec session {len(rec_sessions)}"] = []
 
-            # append path to last session
-            list(rec_sessions.keys())[-1] = path
-            dt_list.append(dt.minute)
+            # add curent time stamp to list
+            dt_list.append(
+                dt.minute
+            )  # maybe this works in for loop lvl instead this level?
+
+        # append path to last session
+        last_key = list(rec_sessions.keys())[-1]
+        rec_sessions[last_key].append(path)  # TODO: check if this works
+
     return rec_sessions
 
 
-# Extract time from filename (TODO: doesn't work for sup folders with this logic !!)
+# Extract time from filename
 def get_timestamps(npz_files):
     """
-    Get start and end time from the filenames of the first and last npz files in one recording directory.
+    Get start and end time from the filenames of the first and last npz files in one recording session (entry in dict).
+    Stores start and end time of each session in a dict with the same keys.
     """
-    # get first and last file names
-    first_file = npz_files[0].name
-    last_file = npz_files[-1].name
+    # old code, working
+    # # get first and last file names
+    # first_file = npz_files[0].name
+    # last_file = npz_files[-1].name
 
-    # get time stamp from file name
-    datetime_str_start = first_file.split("-")[1].split("_")[0]
-    datetime_str_end = last_file.split("-")[1].split("_")[0]
+    # # get time stamp from file name
+    # datetime_str_start = first_file.split("-")[1].split("_")[0]
+    # datetime_str_end = last_file.split("-")[1].split("_")[0]
 
-    # convert to datetime object
-    dt_start = datetime.strptime(datetime_str_start, "%Y%m%dT%H%M%S")
-    dt_end = datetime.strptime(datetime_str_end, "%Y%m%dT%H%M%S")
+    # # convert to datetime object
+    # dt_start = datetime.strptime(datetime_str_start, "%Y%m%dT%H%M%S")
+    # dt_end = datetime.strptime(datetime_str_end, "%Y%m%dT%H%M%S")
 
-    return dt_start, dt_end
+    # ----
+    # create dict to store timestamps of each session
+    time_stamps = {}
+
+    # Iterate over each key in the dictionary
+    for key, file_list in npz_files.items():
+        # Get first and last file names in the current session
+        first_file = file_list[0].name
+        last_file = file_list[-1].name
+
+        # Extract timestamps from file names
+        datetime_str_start = first_file.split("-")[1].split("_")[0]
+        datetime_str_end = last_file.split("-")[1].split("_")[0]
+
+        # Convert to datetime objects
+        dt_start = datetime.strptime(datetime_str_start, "%Y%m%dT%H%M%S")
+        dt_end = datetime.strptime(datetime_str_end, "%Y%m%dT%H%M%S")
+
+        # store start and end time per session as tuple in dict
+        time_stamps[key] = dt_start, dt_end
+
+    return time_stamps
 
 
 # Plot histogram of number of peaks per time bin
@@ -311,7 +368,7 @@ def plot_peaks_time(bin_peaks, binsize):
     """
     Plot histogram of number of peaks per time bin with actual time on x-axis.
     """
-    #### old code, working ####
+    #### old code, working #### TODO: dont hardcode x ticks
     # # Generate time labels for each minute bin
     # time_labels = [
     #     start_time + timedelta(minutes=i * binsize) for i in range(len(peaks))
@@ -332,13 +389,12 @@ def plot_peaks_time(bin_peaks, binsize):
         edgecolor="black",
         align="edge",
     )
-    ax.set_title(f"Number of Peaks per {binsize} min over Time")
+    ax.set_title(f"Number of Peaks over Time (binsize: {binsize} min)")
     ax.set_xlabel("Time [h]")
     ax.set_ylabel("Number of Peaks")
 
     # Set x-ticks to actual time
     ax.set_xticks(list(range(len(bin_peaks))))
-    # embed()
     # Rotate for better readability
     # ax.set_xticklabels(time_labels, rotation=45, ha="right")
 
@@ -353,15 +409,12 @@ binsize = 60  # min
 
 # load data
 file_paths = load_peaks(datapath)
-# min_per_rec = load_wav(datapath)
 
-# get start time from first file
-# start_time = get_timestamps(file_paths)
+# sort file paths into recording sessions
+session_paths = check_sessions(file_paths)
 
 # calculate peaks per bin (TODO: insert binsize in minutes as variable here ?)
-bins = peaks_over_time(binsize, datapath, file_paths, min_per_rec=5)
-
-embed()
+bins = peaks_over_time(binsize, datapath, session_paths)
 
 # plot time histogram
 plot_peaks_time(bins, binsize)
