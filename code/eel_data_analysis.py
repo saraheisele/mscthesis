aad0f06fd5d4e653da1885f4b76dc8b509b19b52
Sarah Eisele <zxoyx50@student.uni-tuebingen.de>
2025-05-27 14:53:36 +0200

    random safety commit

--- code/eel_data_analysis.py
+++ code/eel_data_analysis.py
@@ -18,18 +18,24 @@ and calculates the number of detected peaks per minute/hour and plots the result
 # change peaks_over_time function to store all necessary data in arrays
 # save session idx, path idx (from json), minute, EOD count per minute per rec in npz file
 
-# TODO: modularize peaks_over_time function (for loop in extra function)??
-# TODO: use npz file to to plot sem error bars, (determine certainty)
-# TODO: make rec count y axis in minutes (second yaxis)
+# Monday:
+# plans for brazil
+
+# Tuesday:
+# test which resistor is best for eelgrid data recordings
+# TODO: split script in 2 halves
+# TODO: use npz file to to plot sem error bars (determine certainty)!
+# TODO: make rec count y axis in minutes (second yaxis)!
 # TODO: clean up code
-# TODO: make csv of metadata (automate this ?), to check for correlations
+# TODO: make csv of metadata (automate this w LLM ?)!
+# TODO: check for correlations with external factors (e.g. temperature, salinity, time of day)
 # TODO: peaks over years, months, temp, leitfähigkeit, individuum
 
 # %%
 from rich.console import Console
 from rich.progress import track
 import numpy as np
-from datetime import datetime, timedelta
+from datetime import datetime
 import json
 from pathlib import Path
 import matplotlib.pyplot as plt
@@ -52,15 +58,13 @@ def extract_data(load_path):
     all_paths = [Path(file) for file_list in data.values() for file in file_list]
 
     # extract session id for each recording from key names
-    session_ids = [
-        i for i, file_list in enumerate(data.values()) for file in file_list
-    ]  # TODO: maybe change this to return session structure in all_paths and loop over both sturctures
+    session_ids = [i for i, file_list in enumerate(data.values()) for file in file_list]
 
     return all_paths, session_ids
 
 
-# Extract time from filename
-def get_timestamps(npz_file_path):
+# Extract time from filename (called in peaks_over_time)
+def get_timestamps(npz_file_path, min_per_rec=int):
     """
     Get start and end time from the filenames of the first and last npz files in one recording session.
     """
@@ -72,14 +76,29 @@ def get_timestamps(npz_file_path):
 
     # convert to datetime object
     dt_start = datetime.strptime(datetime_str_start, "%Y%m%dT%H%M%S")
-    dt_end = dt_start + timedelta(minutes=5)
+    # dt_end = dt_start + timedelta(minutes=min_per_rec)
 
-    return dt_start, dt_end
+    # calculate start and end bin of rec
+    start_bin = (dt_start.hour * 60) + dt_start.minute
+    end_bin = start_bin + min_per_rec - 1
+
+    # if start of rec isn't at beginning of a minute ignore peaks in start and end bins
+    if dt_start.second != 0:
+        start_bin += 1
+
+    return start_bin, end_bin, dt_start
 
 
+# Loads one npz file created from patricks find pulses script (called in peaks_over_time)
 def load_npz(file):
     """
-    Load npz file and extract data.
+    Load one npz file, corresponds to one recording.
+    Extract valid peaks and sample rate of this recording.
+    Input:
+        - file: path to npz file
+    Returns:
+        - valid_peaks: np.array of valid peaks (pulses classified as pulse and not noise)
+        - sample_rate: int, sample rate of the recording in Hz
     """
     # load npz file
     with np.load(file) as data:
@@ -92,6 +111,7 @@ def load_npz(file):
     return valid_peaks, sample_rate
 
 
+# TODO: fix progress bar
 # Count how many peaks happen per minute
 def peaks_over_time(
     paths: list,  # list of paths to npz files
@@ -123,19 +143,11 @@ def peaks_over_time(
     ### iterate over each file path in path list
     # track progress with progress bar
     for i, file in track(enumerate(paths)):
-        ### get start and end time of each rec
-        start_time, _ = get_timestamps(file)
+        ### get start and end bin of each rec
+        # only bins that the recording covers in whole are considered, start and end bins are discarded in case rec starts/ends at a non-full minute
+        start_bin, end_bin, start_time = get_timestamps(file)
 
-        # calculate start and end bin of rec
-        start_bin = (start_time.hour * 60) + start_time.minute
-        end_bin = start_bin + min_per_rec - 1
-
-        ### logic to determine correct start and end bin before loop over peaks
-        # if start of rec isn't at beginning of a minute ignore peaks in start and end bins
-        if start_time.second != 0:
-            start_bin += 1
-
-        ## load current file
+        ### load current file
         valid_peaks, sample_rate = load_npz(file)
 
         # calculate data points per minute (sample rate in Hz * 60 sec/min)
@@ -144,7 +156,7 @@ def peaks_over_time(
         # find start index within start bin
         start_idx = int(start_time.second * sample_rate)
 
-        ### initialize counters
+        ### initialize counters for next for loop
         # counter for eod counts per minute per rec
         eod_counter = 0
 
@@ -199,20 +211,6 @@ def peaks_over_time(
     )
 
 
-def save_intermediate(rec_path_ids, session_ids, minutes, eod_per_rec_and_min):
-    """
-    Save intermediate data to npz file.
-    """
-    con.log("Saving intermediate data to npz file.")
-    np.savez(
-        "intermediate_pulse_data.npz",
-        rec_path_ids=rec_path_ids,
-        session_ids=session_ids,
-        minutes=minutes,
-        eod_counts=eod_per_rec_and_min,
-    )
-
-
 def normalize_eods(eods_per_min, rec_per_min):
     """
     Normalize eod count per minute by number of recordings per minute.
@@ -222,8 +220,30 @@ def normalize_eods(eods_per_min, rec_per_min):
     return np.array(eods_per_min) / np.array(rec_per_min)
 
 
+### approach to get eods_permin and rec_per_min from the other variables instead of from peaks_over_time function
+# def normalize_eods(eods_per_rec_and_min, minutes, num_bins):
+#     """
+#     Normalize eod count per minute by number of recordings per minute.
+#     Input: eods_per_min, rec_per_min (lists of eod counts and rec counts per minute)
+#     Output: normalized eod count per minute (array)
+#     """
+#     # initialize arrays
+#     eods_per_min = np.zeros(num_bins)
+#     recs_per_min = np.zeros(num_bins)
+
+
+#     # count occurance of each minute, this is the number of recordings per minute
+#     for i in range(num_bins):
+#         recs_per_min.append(minutes.count(i))
+
+#     # normalize eod counts per minute by number of recordings per minute
+#     normalized_eods = np.array(eods_per_min) / np.array(recs_per_min)
+
+#     return normalized_eods
+
+
 # reshape and sum bins to get eod count per desired bin size (e.g. hour)
-def bin_eods_recs(eods_per_min, rec_per_min, mins_per_timestep=int):
+def bin_eods_recs(eods_per_min, rec_per_min, mins_per_timestep):
     """
     Bin eods per minute into desired timestep. Same for recs per minute
     """
@@ -237,11 +257,41 @@ def bin_eods_recs(eods_per_min, rec_per_min, mins_per_timestep=int):
 
 
 # calculate firing rate per bin/timestep
-def firing_rate_per_bin(eods_per_timestep, mins_per_timestep=int):
+def firing_rate_per_bin(eods_per_timestep, mins_per_timestep):
     """
     Calculates firing rate in Hz per bin.
     """
     return eods_per_timestep / (mins_per_timestep * 60)
+
+
+# save relevant variables from this script to npz file for plotting in diff script
+def save_intermediate(
+    rec_path_ids,
+    session_ids,
+    minutes,
+    eod_per_rec_and_min,
+    eods_per_min_norm,
+    eods_per_bin,
+    recs_per_bin,
+    rate_per_bin,
+    save_path=None,
+):
+    """
+    Save intermediate data to npz file.
+    """
+    if save_path:
+        con.log(f"Saving intermediate data to npz file in {save_path}.")
+        np.savez(
+            save_path,
+            rec_path_id=rec_path_ids,
+            session_id=session_ids,
+            minute=minutes,
+            eods_per_rec_min=eod_per_rec_and_min,
+            eods_per_min_norm=eods_per_min_norm,
+            eods_per_bin_norm=eods_per_bin,
+            recs_per_bin_norm=recs_per_bin,
+            rate_per_bin_norm=rate_per_bin,
+        )
 
 
 # Plot histogram of number of peaks per time bin
@@ -342,6 +392,10 @@ def plot_firing_rate(fr_per_bin, save_path=None):
 
 # %% Main
 def main():
+    # global variables to adjust timescale of analysis
+    timescale_in_minutes = 24 * 60  # 24 hours in minutes
+    minutes_per_bin = 60
+
     # extract data from json file
     data_paths, session_ids = extract_data(
         load_path="/home/eisele/wrk/mscthesis/data/intermediate/eellogger_session_paths.json"
@@ -349,23 +403,34 @@ def main():
 
     # calculate number of eods per minute
     total_eod_counts, rec_counts, rec_paths, sessions, minute_idx, eod_counts = (
-        peaks_over_time(data_paths, session_ids, num_bins=24 * 60, min_per_rec=5)
+        peaks_over_time(data_paths, session_ids, timescale_in_minutes, min_per_rec=5)
     )
-
-    # save data
-    save_intermediate(rec_paths, sessions, minute_idx, eod_counts)
 
     # normalize eod counts per minute by number of recordings per minute
     eod_counts_norm = normalize_eods(total_eod_counts, rec_counts)
 
     # bin eod counts per minute into desired timestep (e.g. hour)
     binned_eod_counts, binned_rec_counts = bin_eods_recs(
-        eod_counts_norm, rec_counts, mins_per_timestep=60
+        eod_counts_norm, rec_counts, minutes_per_bin
     )
 
     # calculate firing rate in Hz for each bin
-    firing_rates = firing_rate_per_bin(binned_eod_counts, mins_per_timestep=60)
+    firing_rates = firing_rate_per_bin(binned_eod_counts, minutes_per_bin)
 
+    # save data
+    save_intermediate(
+        rec_paths,
+        sessions,
+        minute_idx,
+        eod_counts,
+        eod_counts_norm,
+        binned_eod_counts,
+        binned_rec_counts,
+        firing_rates,
+        save_path="/home/eisele/wrk/mscthesis/data/intermediate/intermediate_pulse_data.npz",
+    )
+
+    # ------------ put in seperate script ------------
     # plot eod count histogram
     plot_eod_hist(
         binned_eod_counts,
