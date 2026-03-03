43f1a49bf7c775c0f2216821d40111e419058d07
Sarah Eisele <zxoyx50@student.uni-tuebingen.de>
2026-03-03 15:51:02 +0100

    extracting file duration and start time from wav file now. deleted unnecessary code.

--- code/eel_data_preprocessing.py
+++ code/eel_data_preprocessing.py
@@ -23,28 +23,22 @@ each key contains a list of npz file paths that belong to that session.
 The json file created in this script is loaded and used in the next step of this analysis (eel_data_analysis.py).
 """
 
-# TODO: get list of al hdf files and iterate through them
-# TODO: load wav files and extract sample rates
 # TODO: implement to kick hdf5 file out when it detects 0 predicted pulses (log: found # h5 files, # h5 files are being processed)
-
+# TODO: Maybe store fs, rec length and start time in npz file/dictionary
 # %%
 from rich.console import Console
-from rich.progress import Progress
 from pathlib import Path
 import numpy as np
-from datetime import datetime, timedelta
-import json
 import nixio
 from thunderlab.dataloader import DataLoader
+from datetime import datetime
 # from IPython import embed
-# from audioio.audioloader import AudioLoader
 
 # Initialize console for logging
 con = Console()
 
+
 ### Functions ###
-
-
 def get_path_list(datapath):
     """
     Make a list fo all the paths of hdf5 files from the given file, directory or supdirectory.
@@ -83,8 +77,6 @@ def get_path_list(datapath):
 
 
 # %%
-
-
 def load_eods(file_paths):
     # Print status
     con.log("Loading hdf5 files.")
@@ -105,7 +97,7 @@ def load_eods(file_paths):
 
 
 # %%
-def get_wav(file_paths):
+def find_wav(file_paths):
     """
     Takes the sorted path_list from get_path_list function, that contains one or several hdf5 files.
     Builds the path to the corresponding wavfiles by iterating over h5 files.
@@ -119,11 +111,11 @@ def get_wav(file_paths):
 
     # iterate through hdf5 paths and build path to corresponding wav files
     for fp in file_paths:
-        new_path_name = "_".join(str(fp.name).split("_")[:-1])
+        wav_name = "_".join(str(fp.name).split("_")[:-1])
         wavpath = Path(
             fp.parent.parent.parent.parent
             / "raw/trial_dataset_new/fielddata_trial/inpa2025_trial/inpa_enclosure"
-            / new_path_name
+            / wav_name
         )
 
         # get the first wav file of directory and store the path to it in list
@@ -135,231 +127,63 @@ def get_wav(file_paths):
 
 # %%
 # Extracts sampling rates in Hz from the first corresponding wav files of each hdf5 file and stores them in a list
-def get_fs(wav_path_list):
+def load_wav(wav_path_list):
     fs_list = []
+    dt_list = []
+    length_list = []
 
     # iterate through wav files
     for wav in wav_path_list:
+        # load wav file
         try:
             audio_data = DataLoader(wav)
         except Exception as e:
             con.log(f"Error loading: {e}")
             continue
-        # get sampling rate for this wav file
+
+        ### get sampling rate in Hz for this wav file
         fs = audio_data.rate
         # add sampling rate of each wav to list
         fs_list.append(fs)
 
-    return fs_list
+        ### get file name
+        filename = wav.name
 
+        # get start time of recording from first wav file name (str)
+        start_time = filename.split("-")[1]
 
-# %%
-# Find files that are empty, missing or somehow corrupted
-def faulty_files(wav_path_list, npz_path_list):
-    """
-    Check if there are any missing or empty files in the given lists of wav and npz files.
-    """
-    # print status
-    con.log("Checking for empty/missing/corrupted files.")
+        # convert to datetime object
+        dt_start = datetime.strptime(start_time, "%Y%m%dT%H%M%S")
 
-    # add progress bar
-    with Progress() as progress:
-        task = progress.add_task(
-            "Checking for faulty files...", total=len(npz_path_list)
-        )
+        # add start time datetime object to list
+        dt_list.append(dt_start)
 
-        ## check if the wav and npz files have the same names, i.e. if npz file exists for each wav file
-        for i, (wav_file, npz_file) in enumerate(zip(wav_path_list, npz_path_list)):
-            # get the filenames without "peaks" and suffixes
-            wav_name = wav_file.stem
-            npz_name = npz_file.stem.split("_")[0]
-            if wav_name != npz_name:
-                con.log(f"Warning: No npz file that matches wav file {wav_file.name}!")
-                # insert NaN in place of missing npz file
-                npz_path_list.insert(i, None)
+        ### get file length
+        # get number of minutes per recording for this wav file
+        samples_per_rec = audio_data.shape[0]
+        minutes_rec = samples_per_rec / fs / 60
 
-            ## check if existing npz file is empty
-            # check for file size
-            elif Path(npz_file).stat().st_size == 0:
-                con.log(f"Warning: npz file {npz_file.name} is empty!")
-                # insert NaN in place of empty npz file
-                npz_path_list[i] = None
+        # add recording length in minutes to list
+        length_list.append(minutes_rec)
 
-            # if file size isnt zero, check if npz file contains npy files (dict keys)
-            else:
-                try:
-                    with np.load(npz_file) as data:
-                        # check if npz file contains any keys
-                        if len(data.files) == 0:
-                            con.log(
-                                f"Warning: npz file {npz_file.name} contains no keys!"
-                            )
-                            # insert NaN in place of empty npz file
-                            npz_path_list[i] = None
+    return fs_list, dt_list, length_list
 
-                        # check if predicted_labels exists (crucial for analysis)
-                        elif "predicted_labels" not in data.files:
-                            con.log(
-                                f"Warning: npz file {npz_file.name} does not contain predicted_labels key!"
-                            )
-                            # insert NaN in place of file without predicted_labels
-                            npz_path_list[i] = None
+    # %% Main
+    # def main():
+    # Path to sup folder
+    datapath = Path("/home/eisele/wrk/mscthesis/data/intermediate/inpa2025_peaks")
 
-                        # if keys exist, check if keys are empty
-                        else:
-                            for key in data.files:
-                                if data[key].size == 0:
-                                    con.log(
-                                        f"Warning: npz file {npz_file.name} contains empty key {key}!"
-                                    )
-                except Exception as e:
-                    con.log(f"Error loading {npz_file.name}: {e}")
-                    # insert NaN in place of empty npz file
-                    npz_path_list[i] = None
+    # make list containing all paths to hdf5 files in the given datapath
+    path_list = get_path_list(datapath)
 
-            # update progress bar
-            progress.update(task, advance=1)
+    # load hdf5 files from path list and extract pulse centers and predicted labels
+    pulse_center_idc, pred_labels = load_eods(path_list)
 
-    return npz_path_list
+    # make list of the first wav file of each recording session corresponding to the hdf5 files in path_list
+    wav_list = find_wav(path_list)
 
-
-# Sort file paths into recording sessions
-def check_sessions(file_paths: list):
-    """
-    Check if recordings are subsequent and store seperate recording sessions in a dictionary.
-    """
-    ## Pair each path with its extracted datetime or None
-    # initiate empty list to store tuples of (datetime, path) --- TODO: make this for loop a seperate function
-    dt_path_list = []
-
-    for path in file_paths:
-        # append Nones for currupted files to preserve order
-        if path is None:
-            dt_path_list.append((None, None))
-        # valid files
-        else:
-            # get time stamp from file name
-            time_stamp = path.name.split("-")[1].split("_")[0]
-            dt = datetime.strptime(time_stamp, "%Y%m%dT%H%M%S")
-            # add path together with its timestamp to list
-            dt_path_list.append((dt, path))
-
-    ##
-    # accepted time difference and tolerance between two files
-    t_diff = timedelta(minutes=5)
-    tolerance = timedelta(seconds=1)
-
-    # store recording sessions in a dictionary
-    rec_sessions = {}
-    current_session = []
-
-    # set last valid timestamp to None to indicate first session
-    last_valid_dt = None
-
-    # count number of subsequent Nones
-    none_counter = 0
-
-    # iterate over paths and corresponding timestamps
-    for dt, path in dt_path_list:
-        # skip corrupted files
-        if dt is None:
-            # increase counter for each subsequent None
-            none_counter += 1
-            continue
-
-        # at first valid entry, start first session
-        if last_valid_dt is None:
-            # add first path to session
-            current_session = [path]
-
-        # all other valid entries
-        else:
-            # check time difference to last valid timestamp
-            time_gap = dt - last_valid_dt
-            # calculate how big expected gap is based on how many files were skipped
-            expected_gap = t_diff * (none_counter + 1)
-
-            # if current rec is subsequent to the last valid (non-None) rec
-            if abs(time_gap - expected_gap) <= tolerance:
-                # add to the same session
-                current_session.append(path)
-
-            # if current rec not subsequent
-            else:
-                # add last session to dict
-                session_key = f"rec session {len(rec_sessions)}"
-                rec_sessions[session_key] = current_session
-                # start a new session
-                current_session = [path]
-
-        # update last valid timestamp
-        last_valid_dt = dt
-        # reset none counter
-        none_counter = 0
-
-    # save the final session
-    if current_session:
-        session_key = f"rec session {len(rec_sessions)}"
-        rec_sessions[session_key] = current_session
-
-    return rec_sessions
-
-
-# Save dictionary that contains lists of paths per rec session in json file
-def save_session_paths(session_paths):
-    """
-    Save the session paths to a JSON file.
-    """
-    # define path where json file is created
-    path = Path(
-        "/home/eisele/wrk/mscthesis/data/intermediate/eellogger_session_paths.json"
-    )
-
-    # Convert Path objects to strings
-    session_paths_str = {k: [str(p) for p in v] for k, v in session_paths.items()}
-
-    # print statement before saving
-    con.log(f"Saving session paths to {path}.")
-
-    # open json and save dict in it
-    with path.open("w") as file:
-        json.dump(session_paths_str, file)
-
-
-# %%
-### Import Data ###
-
-## Datapath to npz files (folder, subfolder or file)
-# datapath = Path(
-#     "/home/eisele/wrk/mscthesis/data/raw/eellogger_example_data_peaks/recordings2025-03-06/eellogger1-20250306T101402_peaks.npz"
-# )
-
-# # Path to folder
-# datapath = Path(
-#     "/home/eisele/wrk/mscthesis/data/raw/eellogger_example_data_peaks/recordings2025-03-06/"
-# )
-
-
-# %% Main
-# def main():
-#     # Path to sup folder
-#     datapath = Path("/home/eisele/wrk/mscthesis/data/intermediate/inpa2025_peaks")
-
-#     # make list containing all paths to hdf5 files in the given datapath
-#     path_list = get_path_list(datapath)
-
-#     # load hdf5 files from path list and extract pulse centers and predicted labels
-#     pulses_center_idx, pred_labels = load_eods(path_list)
-
-
-#     # # faulty files function
-#     # path_list_new = faulty_files(path_list)
-
-#     # # sort file paths into recording sessions
-#     # session_paths = check_sessions(path_list_new)
-
-#     # # save session paths to json file
-#     # save_session_paths(session_paths)
+    # get sampling rates from the first wav file of each recording session
+    sampling_rates, start_times, rec_lengths = load_wav(wav_list)
 
 
 # if __name__ == "__main__":
--- code/eel_data_storage_code.py
+++ code/eel_data_storage_code.py
@@ -1,8 +1,12 @@
 """This script contains functions from my eel_data processing pipeline that I dont need anymore/right now."""
 
+import numpy as np
 from rich.console import Console
+from rich.progress import Progress
 from pathlib import Path
 import re
+from datetime import datetime, timedelta
+import json
 
 
 # Initialize console for logging
@@ -89,3 +93,189 @@ def load_wav(datapath):
 
 
 # wav_paths = load_wav(datapath)
+
+
+# Find files that are empty, missing or somehow corrupted
+def faulty_files(wav_path_list, npz_path_list):
+    """
+    Check if there are any missing or empty files in the given lists of wav and npz files.
+    """
+    # print status
+    con.log("Checking for empty/missing/corrupted files.")
+
+    # add progress bar
+    with Progress() as progress:
+        task = progress.add_task(
+            "Checking for faulty files...", total=len(npz_path_list)
+        )
+
+        ## check if the wav and npz files have the same names, i.e. if npz file exists for each wav file
+        for i, (wav_file, npz_file) in enumerate(zip(wav_path_list, npz_path_list)):
+            # get the filenames without "peaks" and suffixes
+            wav_name = wav_file.stem
+            npz_name = npz_file.stem.split("_")[0]
+            if wav_name != npz_name:
+                con.log(f"Warning: No npz file that matches wav file {wav_file.name}!")
+                # insert NaN in place of missing npz file
+                npz_path_list.insert(i, None)
+
+            ## check if existing npz file is empty
+            # check for file size
+            elif Path(npz_file).stat().st_size == 0:
+                con.log(f"Warning: npz file {npz_file.name} is empty!")
+                # insert NaN in place of empty npz file
+                npz_path_list[i] = None
+
+            # if file size isnt zero, check if npz file contains npy files (dict keys)
+            else:
+                try:
+                    with np.load(npz_file) as data:
+                        # check if npz file contains any keys
+                        if len(data.files) == 0:
+                            con.log(
+                                f"Warning: npz file {npz_file.name} contains no keys!"
+                            )
+                            # insert NaN in place of empty npz file
+                            npz_path_list[i] = None
+
+                        # check if predicted_labels exists (crucial for analysis)
+                        elif "predicted_labels" not in data.files:
+                            con.log(
+                                f"Warning: npz file {npz_file.name} does not contain predicted_labels key!"
+                            )
+                            # insert NaN in place of file without predicted_labels
+                            npz_path_list[i] = None
+
+                        # if keys exist, check if keys are empty
+                        else:
+                            for key in data.files:
+                                if data[key].size == 0:
+                                    con.log(
+                                        f"Warning: npz file {npz_file.name} contains empty key {key}!"
+                                    )
+                except Exception as e:
+                    con.log(f"Error loading {npz_file.name}: {e}")
+                    # insert NaN in place of empty npz file
+                    npz_path_list[i] = None
+
+            # update progress bar
+            progress.update(task, advance=1)
+
+    return npz_path_list
+
+
+# Sort file paths into recording sessions
+def check_sessions(file_paths: list):
+    """
+    Check if recordings are subsequent and store seperate recording sessions in a dictionary.
+    """
+    ## Pair each path with its extracted datetime or None
+    # initiate empty list to store tuples of (datetime, path) --- TODO: make this for loop a seperate function
+    dt_path_list = []
+
+    for path in file_paths:
+        # append Nones for currupted files to preserve order
+        if path is None:
+            dt_path_list.append((None, None))
+        # valid files
+        else:
+            # get time stamp from file name
+            time_stamp = path.name.split("-")[1].split("_")[0]
+            dt = datetime.strptime(time_stamp, "%Y%m%dT%H%M%S")
+            # add path together with its timestamp to list
+            dt_path_list.append((dt, path))
+
+    ##
+    # accepted time difference and tolerance between two files
+    t_diff = timedelta(minutes=5)
+    tolerance = timedelta(seconds=1)
+
+    # store recording sessions in a dictionary
+    rec_sessions = {}
+    current_session = []
+
+    # set last valid timestamp to None to indicate first session
+    last_valid_dt = None
+
+    # count number of subsequent Nones
+    none_counter = 0
+
+    # iterate over paths and corresponding timestamps
+    for dt, path in dt_path_list:
+        # skip corrupted files
+        if dt is None:
+            # increase counter for each subsequent None
+            none_counter += 1
+            continue
+
+        # at first valid entry, start first session
+        if last_valid_dt is None:
+            # add first path to session
+            current_session = [path]
+
+        # all other valid entries
+        else:
+            # check time difference to last valid timestamp
+            time_gap = dt - last_valid_dt
+            # calculate how big expected gap is based on how many files were skipped
+            expected_gap = t_diff * (none_counter + 1)
+
+            # if current rec is subsequent to the last valid (non-None) rec
+            if abs(time_gap - expected_gap) <= tolerance:
+                # add to the same session
+                current_session.append(path)
+
+            # if current rec not subsequent
+            else:
+                # add last session to dict
+                session_key = f"rec session {len(rec_sessions)}"
+                rec_sessions[session_key] = current_session
+                # start a new session
+                current_session = [path]
+
+        # update last valid timestamp
+        last_valid_dt = dt
+        # reset none counter
+        none_counter = 0
+
+    # save the final session
+    if current_session:
+        session_key = f"rec session {len(rec_sessions)}"
+        rec_sessions[session_key] = current_session
+
+    return rec_sessions
+
+
+# Save dictionary that contains lists of paths per rec session in json file
+def save_session_paths(session_paths):
+    """
+    Save the session paths to a JSON file.
+    """
+    # define path where json file is created
+    path = Path(
+        "/home/eisele/wrk/mscthesis/data/intermediate/eellogger_session_paths.json"
+    )
+
+    # Convert Path objects to strings
+    session_paths_str = {k: [str(p) for p in v] for k, v in session_paths.items()}
+
+    # print statement before saving
+    con.log(f"Saving session paths to {path}.")
+
+    # open json and save dict in it
+    with path.open("w") as file:
+        json.dump(session_paths_str, file)
+
+
+# %%
+### Import Data ###
+
+## Datapath to npz files (folder, subfolder or file)
+# datapath = Path(
+#     "/home/eisele/wrk/mscthesis/data/raw/eellogger_example_data_peaks/recordings2025-03-06/eellogger1-20250306T101402_peaks.npz"
+# )
+
+# # Path to folder
+# datapath = Path(
+#     "/home/eisele/wrk/mscthesis/data/raw/eellogger_example_data_peaks/recordings2025-03-06/"
+# )
